04aac11a0befd1a65b5f35817b68973e31a39820
alfa9317 <josealfredotg@gmail.com>
2020-03-09 06:39:39 -0600

    Take out container of home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -18,21 +18,21 @@ class Home extends React.Component {
                         <h1 className='homePageTitle'>¡Welcome!</h1>
                     </div>
                 </Animated>
-                {/* <div className='container'>
-                    <Animated animationIn="fadeIn" animationOut="fadeOut" animationInDelay={0.4} animationInDuration={3000} isVisible={true}>
-                        <img alt="profilePic" src={profilePic} style={{width: '200px',height: 'auto',borderRadius: '150px', boxShadow: '0px 4px 15px 2px #888888'}}/>
-                        <br/>
-                        <br/>
-                        <br/>
-                    </Animated>
-                    <Animated animationIn="fadeIn" animationOut="fadeOut" animationInDelay={0.6} animationInDuration={4000} isVisible={true}>
-                        <p>My name is José Alfredo Torres</p>
-                    </Animated>
-                    <Animated animationIn="fadeIn" animationOut="fadeOut" animationInDelay={0.7} animationInDuration={4000} isVisible={true}>
-                        <p style={{margin:'20px 100px 0px 100px'}}>I'm a Full Stack Web and iOS Developer with a background in Sound Design, from Tec de Monterrey University. Effective at combining rational and objective analysis with creative and inventive solutions to develop beautiful and efficient user-friendly applications.</p>
-                    </Animated>
-                </div>
-                <div className='homePageTitle' style={{fontSize:'100px'}}>...</div> */}
+                
+                <Animated animationIn="fadeIn" animationOut="fadeOut" animationInDelay={0.4} animationInDuration={3000} isVisible={true}>
+                    <img alt="profilePic" src={profilePic} style={{width: '200px',height: 'auto',borderRadius: '150px', boxShadow: '0px 4px 15px 2px #888888'}}/>
+                    <br/>
+                    <br/>
+                    <br/>
+                </Animated>
+                <Animated animationIn="fadeIn" animationOut="fadeOut" animationInDelay={0.6} animationInDuration={4000} isVisible={true}>
+                    <p>My name is José Alfredo Torres</p>
+                </Animated>
+                <Animated animationIn="fadeIn" animationOut="fadeOut" animationInDelay={0.7} animationInDuration={4000} isVisible={true}>
+                    <p style={{margin:'200px 100px 0px 200px'}}>I'm a Full Stack Web and iOS Developer with a background in Sound Design, from Tec de Monterrey University. Effective at combining rational and objective analysis with creative and inventive solutions to develop beautiful and efficient user-friendly applications.</p>
+                </Animated>
+             
+                <div className='homePageTitle' style={{fontSize:'100px'}}>...</div>
             </div>
             
           );
