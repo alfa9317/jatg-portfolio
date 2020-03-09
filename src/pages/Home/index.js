73d34cff951b88a4b9ae23e75443dcd57cf5b153
alfa9317 <josealfredotg@gmail.com>
2020-03-09 04:05:53 -0600

    Add again antd functionality

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,27 +1,28 @@
 import React from "react";
 import "./style.css";
 // import {Animated} from "react-animated-css";
-// import { Button } from 'antd';
+import { Button } from 'antd';
 
 
 
 class Home extends React.Component {
     render(){
         return (
-            // <div className="card">
-            //      <Animated animationIn="fadeIn" animationOut="fadeOut" animationInDuration={4000} isVisible={true}>
-            //         <div>
-            //             <h1>Hello world! ;)</h1>
-            //         </div>
-            //     </Animated>
-            //     <div>
-            //         <Button type="primary">Primary</Button>
-            //         <Button>Default</Button>
-            //         <Button type="dashed">Dashed</Button>
-            //         <Button type="link">Link</Button>
-            //     </div>
-            // </div>
-            <h1>Hola! Aquí funcionando</h1>
+            <div className="card">
+                 {/* <Animated animationIn="fadeIn" animationOut="fadeOut" animationInDuration={4000} isVisible={true}>
+                    <div>
+                        <h1>Hello world! ;)</h1>
+                    </div>
+                </Animated> */}
+                <div>
+                    <Button type="primary">Primary</Button>
+                    <Button>Default</Button>
+                    <Button type="dashed">Dashed</Button>
+                    <Button type="link">Link</Button>
+                </div>
+                <h1>Hola! Aquí funcionando</h1>
+            </div>
+            
           );
     }
   }
