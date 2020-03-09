645de6170605a33db9c052e6bc570ee8f67e510e
alfa9317 <josealfredotg@gmail.com>
2020-03-09 03:53:43 -0600

    Still trying to fix heroku

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./style.css";
-import { Link } from "react-router-dom";
-import SignOutButton from '../SignOut/index';
-import logo from "./logo.png"
-import profilePic from "./profilePic.jpg"
+// import { Link } from "react-router-dom";
+// import SignOutButton from '../SignOut/index';
+// import logo from "./logo.png"
+// import profilePic from "./profilePic.jpg"
 
 
 function NavBar(props) {
     return (
         <div>
-           
+           <h1>Navbar</h1>
         </div>
     );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,27 +1,27 @@
 import React from "react";
 import "./style.css";
-import {Animated} from "react-animated-css";
-import { Button } from 'antd';
+// import {Animated} from "react-animated-css";
+// import { Button } from 'antd';
 
 
 
 class Home extends React.Component {
     render(){
         return (
-            <div className="card">
-                 <Animated animationIn="fadeIn" animationOut="fadeOut" animationInDuration={4000} isVisible={true}>
-                    <div>
-                        <h1>Hello world! ;)</h1>
-                    </div>
-                </Animated>
-                <div>
-                    <Button type="primary">Primary</Button>
-                    <Button>Default</Button>
-                    <Button type="dashed">Dashed</Button>
-                    <Button type="link">Link</Button>
-                </div>
-            </div>
-
+            // <div className="card">
+            //      <Animated animationIn="fadeIn" animationOut="fadeOut" animationInDuration={4000} isVisible={true}>
+            //         <div>
+            //             <h1>Hello world! ;)</h1>
+            //         </div>
+            //     </Animated>
+            //     <div>
+            //         <Button type="primary">Primary</Button>
+            //         <Button>Default</Button>
+            //         <Button type="dashed">Dashed</Button>
+            //         <Button type="link">Link</Button>
+            //     </div>
+            // </div>
+            <h1>Hola! Aquí funcionando</h1>
           );
     }
   }
