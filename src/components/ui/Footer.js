9c8f66632d4e4056eda55efaf40aff94ea491524
alfa9317 <josealfredotg@gmail.com>
2020-07-01 21:53:46 -0500

    Footer adornment

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
+import footerAdornment from '../../assets/images/Footer_start.png';
 
 const useStyles = makeStyles(theme => ({
     footer: {
-        backgroundColor: theme.palette.common.blue,
+        backgroundColor: theme.palette.common.darkBlue,
         width: '100%' 
+    },
+    adornment: {
+        width: '22em'
     }
 }))
 
 export default function Footer(){
     const classes = useStyles()
 
-    return <footer className={classes.footer}>José Alfredo Torres, 2020</footer>
+    return (
+        <footer className={classes.footer}>
+            <img alt='footbar decoration' src={footerAdornment} className={classes.adornment}/>
+         </footer>
+    );
+        
 }
